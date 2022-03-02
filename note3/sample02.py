ccae2265420c54556b25a71b41cd76977d282b4f
Hirotoshi Kawakami <59823253+Hirotoshi0919@users.noreply.github.com>
2022-03-02 17:19:09 +0900

    フォルダ作成、コード追加

--- note3/sample02.py
+++ note3/sample02.py
@@ -1,9 +1,11 @@
-# sample01.py
+# sample02.py
 number = input("数字を入力してください\n")
 # 文字型のnumberを数値型に変換する
 number = int(number)
 
 if number == 1:
     print("変数numberは1と等しいです")
+else:
+    print("変数numberは1以外の値です")
 
 print("終了しました")
--- note3/sample03.py
+++ note3/sample03.py
@@ -1,9 +1,15 @@
-# sample01.py
+# sample03.py
 number = input("数字を入力してください\n")
 # 文字型のnumberを数値型に変換する
 number = int(number)
 
 if number == 1:
     print("変数numberは1と等しいです")
+elif number == 2:
+    print("変数numberは2と等しいです")
+elif number == 100:
+    print("変数numberは100と等しいです")
+else:
+    print("変数numberは1、2、100以外の値です")
 
 print("終了しました")
